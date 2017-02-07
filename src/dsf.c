acf7dc40b22b8832c935e905c068388c45793a42
marco <marco@pc>
2017-02-07 22:09:46 +0100

    Mods to allow streaming of dsf files during real time conversion. This pathc was taken by Daphile but cleaned up a bit.

--- src/dsf.c
+++ src/dsf.c
@@ -229,7 +229,7 @@ static int dsf_writeheader(sox_format_t *ft)
 	    lsx_writedw(ft, dsf->sfreq) ||
 	    lsx_writedw(ft, dsf->bps) ||
 	    // lsx_writeqw(ft, dsf->scount) ||
-	    lsx_writeqw(ft, dsf->scount ? dsf->scount : UINT64_MAX)
+	    lsx_writeqw(ft, dsf->scount ? dsf->scount : UINT64_MAX) ||
 	    lsx_writedw(ft, dsf->block_size) ||
 	    lsx_writedw(ft, 0) || /* reserved */
 	    lsx_writedw(ft, DATA_TAG) ||
